3aca687373ab884179bf81a09ce26b63214ac1fd
regdocs <55305804+regdocs@users.noreply.github.com>
2024-08-18 21:08:19 +0530

    Add first input filter
    Co-authored-by: Stuti Pradhan <StutiPradhan@users.noreply.github.com>

--- tokeniser/src/main/java/dev/bibliotheca/tokeniser/Tokeniser.java
+++ tokeniser/src/main/java/dev/bibliotheca/tokeniser/Tokeniser.java
@@ -1,7 +1,19 @@
 package dev.bibliotheca.tokeniser;
 
+import java.util.Scanner;
+
+import dev.bibliotheca.tokeniser.filters.InputFilter;
+import dev.bibliotheca.tokeniser.filters.punctuation.PeriodFilter;
+
 public class Tokeniser {
     public static void main(String[] args) {
-        System.out.println("Hello tokeniser!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your input: ");
+        String userInput = sc.nextLine();
+
+        InputFilter periodFilter = new PeriodFilter(userInput);
+        System.out.println(periodFilter.filteredInput);
+
+        sc.close();
     }
 }
