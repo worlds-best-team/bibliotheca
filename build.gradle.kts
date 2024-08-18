9aab22f30b68e79eaf8b8fc8ecac8c4901be6e2e
Stuti Pradhan <stutipradhan02@gmail.com>
2024-08-18 21:13:38 +0530

    Initialised

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
-group = "dev.worldsbestteam"
+group = "dev.bibliotheca"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +14,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
